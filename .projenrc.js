0dd12a461ae872e4478559afe85680bd89e98e36
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-02-07 07:45:09 -0500

    projen init

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,24 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'iam-pipeline',
+    author: 'Shawn Torsitano',
+    authorAddress: 'shawn.torsitano@gmail.com',
+    cdkVersion: '2.9.0',
+    defaultReleaseBranch: 'main',
+    name: 'iam-pipeline',
+    eslint: false,
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            useTabs: false,
+            tabWidth: 4,
+            printWidth: 120,
+        },
+    },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+project.setScript('format', 'prettier -w "src/**/*.ts"');
 project.synth();
