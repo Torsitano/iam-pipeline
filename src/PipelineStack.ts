1b75bfcbd8c6a76f68c80b0392583a33ea5e8a7c
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-02-14 07:52:17 -0500

    changing to codestar connection

--- src/PipelineStack.ts
+++ src/PipelineStack.ts
@@ -9,7 +9,9 @@ export class PipelineStack extends Stack {
       new CodePipeline( this, 'Pipeline', {
           pipelineName: 'TestPipeline',
           synth: new ShellStep('Synth', {
-              input: CodePipelineSource.gitHub('Torsitano/iam-pipeline', 'main'),
+              input: CodePipelineSource.connection('Torsitano/iam-pipeline', 'main', {
+                  connectionArn: 'arn:aws:codestar-connections:us-east-1:698852667105:connection/9c887503-78c4-4111-9cb4-6d2b3854016e'
+              }),
               commands: [ 'npm ci', 'npm run build', 'npx cdk synth' ]
           })
       })
