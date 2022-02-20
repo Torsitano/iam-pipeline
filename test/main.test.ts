3152fba5c318fc60e784c3eaf0de9b20058d2888
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-02-20 07:31:45 -0500

    Adding env to test

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,7 +4,12 @@ import { PipelineStack } from '../src/PipelineStack';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new PipelineStack(app, 'test');
+  const stack = new PipelineStack(app, 'test', {
+      env: {
+        account: '698852667105',
+        region: 'us-east-1',
+      }
+  });
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
